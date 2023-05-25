1a44d31c9d97250bf0015be3ce0f78331ff143ad
João Vitor <100807853+jvvtr@users.noreply.github.com>
2023-05-25 09:41:42 -0300

    Add files via upload

---
+++ PontoEletronico/src/view/Func.java
@@ -0,0 +1,21 @@
+package view;
+
+
+public class Func {
+    private String matricula;
+
+    public Func(String matriculaFunc){
+        
+        this.matricula = matriculaFunc;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    
+}
